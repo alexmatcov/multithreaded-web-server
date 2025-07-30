f8db143d653c7ffe2d8681dcd2a40a601ec875d2
Alex Matcov <alex.matcov@gmail.com>
2025-07-30 17:18:59 +0200

    respond to unexpected requests

--- src/main.rs
+++ src/main.rs
@@ -20,23 +20,31 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        // returns an iterator of Result<String, std::io::Error> by splitting the stream at a newline byte 
-        .lines()
-        // get each String by unwrap() each Result
-        .map(|result| result.unwrap())
-        // browser signals the end of an HTTP request by sending 2 newline chars in a row
-        .take_while(|line| !line.is_empty())
-        .collect();
-        
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    // first unwrap() takes care of the option and takes care if the iterator has no items,
+    // the second take the String from the Result
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // as_bytes() converts the string data to bytes.
-    // write_all() takes a &[u8] and sens the bytes directly down the connection
-    stream.write_all(response.as_bytes()).unwrap();
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        // as_bytes() converts the string data to bytes.
+        // write_all() takes a &[u8] and sens the bytes directly down the connection
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 
 }
