8b463826aa12b376dfb2bca9005e726d47a19fd4
Alex Matcov <alex.matcov@gmail.com>
2025-07-30 16:55:24 +0200

    print incoming requests

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,21 @@ fn main() {
         // unwrap() terminates the program if the stream has errors
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream)
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        // returns an iterator of Result<String, std::io::Error> by splitting the stream at a newline byte 
+        .lines()
+        // get each String by unwrap() each Result
+        .map(|result| result.unwrap())
+        // browser signals the end of an HTTP request by sending 2 newline chars in a row
+        .take_while(|line| !line.is_empty())
+        .collect();
+        
+    println!("Request: {http_request:#?}");
+
 }
